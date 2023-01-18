66948a5564e3f8191fc60d9c1a74c19b515077aa
jupijee <jjipp830308@gmail.com>
2023-01-18 23:39:41 +0100

    Zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Justyna";
+string sex = "kobieta";
+int age = 39;
+if(sex=="kobieta" && age<30)
+{ Console.WriteLine("Kobieta poniżej 30 lat"); }
+if (age == 39) 
+{ if (name=="Justyna")
+    { Console.WriteLine("Justyna lat 39"); }
+}
+if(!(sex=="kobieta"))
+    { if (age<18)
+    { Console.WriteLine("Niepełnoletni mężczyzna"); }
+}
