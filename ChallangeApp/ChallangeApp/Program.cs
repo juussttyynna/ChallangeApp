7f0eecd83928bc9ca29b0f60e851505c3ebe2909
jupijee <jjipp830308@gmail.com>
2023-01-19 21:33:29 +0100

    Zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,22 +1,16 @@
-﻿string name = "Ewa";
-string sex = "kobieta";
+﻿var name = "Justyna";
+var sex = "kobieta";
 int age = 33;
-if(sex=="kobieta" && age<30)
-{ Console.WriteLine("Kobieta poniżej 30 lat");
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30");
 }
-else { Console.WriteLine("Mężczyzna powyżej 30 lat"); 
+else if (name == "Justyna" && age == 33)
+{
+    Console.WriteLine("Justyna lat 33");
 }
-if (age == 33) 
-{ if (name=="Ewa")
-    { Console.WriteLine("Ewa lat 33"); 
-    }
-}
-else { Console.WriteLine ("Kobieta lat 30"); 
-}
-if(!(sex=="kobieta"))
-    { if (age<18)
-    { Console.WriteLine("Niepełnoletni mężczyzna"); 
-    }
-}
-else { Console.WriteLine("Pełnoletni mężczyzna");
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
 }
