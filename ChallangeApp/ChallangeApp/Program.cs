7537b13a69b21e4b9a125edce735d7a3a476610d
jupijee <jjipp830308@gmail.com>
2023-01-21 17:53:54 +0100

    zadanie domowe dzień 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,16 +1,26 @@
-﻿var name = "Justyna";
-var sex = "kobieta";
-int age = 33;
+﻿int number = 1899568222;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray(); 
 
-if (sex == "kobieta" && age < 30)
+List<char> numbers = new List<char>();
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+
+foreach (var digit in numbers)
 {
-    Console.WriteLine("Kobieta poniżej 30");
-}
-else if (name == "Justyna" && age == 33)
-{
-    Console.WriteLine("Justyna lat 33");
-}
-else if (sex == "mężczyzna" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    var count = 0;
+    
+    foreach (var x in letters) 
+    {
+        if (digit == x) count++;
+    }
+    Console.WriteLine(digit + "=>" + count++);
+    
 }
